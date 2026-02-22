2defc46857734a1c9bfecdf733f7430a72e5ff8d
Leslie Lopez <leslie2515lopez@gmail.com>
2026-02-21 23:59:12 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
 import "dotenv/config";
 import mongoose from "moongoose";
 import cors from "cors";
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
-app.use(cors({
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-}))
+  }),
+);
 
-app.listen(7000, () => {
-    console.log("Server running on localhost:7000");
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
