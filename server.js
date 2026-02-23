2dfebe71bc5f5e5d4831bed194103d00a65686d7
Leslie Lopez <leslie2515lopez@gmail.com>
2026-02-22 22:22:19 -0600

    Added mongodb and mongoose to server.js

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-import express, { Request, Response } from "express";
-import "dotenv/config";
-import mongoose from "moongoose";
+import { setServers } from "node:dns/promises";
+
+setServers(["1.1.1.1", "8.8.8.8"]);
+
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import cors from "cors";
-require("dotenv").config();
+
+dotenv.config();
+
+const MONGODB_URL = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 app.use(express.json());
@@ -13,6 +25,10 @@ app.use(
     credentials: true,
   }),
 );
+
+app.get("/", (req, res) => {
+  res.send("Blog API is running");
+});
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
