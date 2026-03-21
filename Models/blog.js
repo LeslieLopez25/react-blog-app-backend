b2dbe29699134e2d4d113ceaa29f910d7edb8e9d
Leslie Lopez <leslie2515lopez@gmail.com>
2026-03-21 00:12:28 -0600

    Created blog schema

---
+++ Models/blog.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    images: [
+      {
+        url: String,
+        filename: String,
+      },
+    ],
+    notes: [
+      {
+        content: String,
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["idea", "in-progress", "completed"],
+      default: "idea",
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Blog", blogSchema);
