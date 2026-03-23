312fd7a51212b2efff41c8dbd8736ff7b6800fa9
Leslie Lopez <leslie2515lopez@gmail.com>
2026-03-22 23:53:40 -0600

    Created user controller and updated blog controller

--- Controllers/blogController.js
+++ Controllers/blogController.js
@@ -3,8 +3,10 @@ import Blog from "../Models/blog";
 export const createBlog = async (req, res) => {
   try {
     const { title, description } = req.body;
-    if (!title) {
-      return res.status(400).json({ message: "Title is required" });
+    if (!title || !description) {
+      return res
+        .status(400)
+        .json({ message: "Title and description are required" });
     }
 
     const blog = new Blog({
